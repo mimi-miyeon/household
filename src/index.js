352dcf9d5102d1365ef40d55a9f46920f5d55f67
Miyeon Mimi Yang <miyeon0403@hotmail.com>
2024-02-04 15:12:17 +0900

    gh-pages and BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter basename="https://mimi-miyeon.github.io/mimispace1/site/household">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
